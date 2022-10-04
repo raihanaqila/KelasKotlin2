f589ae2114a99d1a46e4784e2b123b8b16a1e131
raihanaqila <raihan.aqila.tkom419@polban.ac.id>
2022-10-04 16:33:51 +0700

    Latihan-1

---
+++ src/main/kotlin/Latihan.kt
@@ -0,0 +1,34 @@
+fun main()
+{
+    print("Masukan Lama Parkir (Jam): ")
+
+    var lamaParkir = Integer.valueOf(readLine())
+    var biayaParkir: Float
+    var biayaParkir2: Float
+    var biayaParkir3: Float
+
+    // rule biaya parkir
+
+    val tarifDasar = 1.0
+    val tarifBerikutnya = 0.5
+    val maxTarifPerHari = 15
+
+    if (lamaParkir in 0..5)
+    {
+        biayaParkir = tarifDasar.toFloat()*lamaParkir
+    }
+    else if (lamaParkir > 5 && lamaParkir < 24)
+    {
+        biayaParkir2 = (lamaParkir-5).toFloat()
+        biayaParkir = 5+(tarifBerikutnya.toFloat()*biayaParkir2)
+    }
+    else
+    {
+        biayaParkir3 = (lamaParkir/24).toFloat()
+        biayaParkir2 = (biayaParkir3*15).toFloat()
+        biayaParkir = (biayaParkir3*maxTarifPerHari).toFloat()+(tarifBerikutnya.toFloat()*biayaParkir2)
+    }
+
+    println("Lama Parkir: $lamaParkir")
+    println("Biaya Parkir: $biayaParkir")
+}
