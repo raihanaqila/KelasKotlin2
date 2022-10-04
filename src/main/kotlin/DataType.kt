beabf2f4184888fd7a12ce0a6aaa11f919f8ced0
raihanaqila <raihan.aqila.tkom419@polban.ac.id>
2022-10-04 15:38:35 +0700

    latihan

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    val dataInt = 5
+    val dataString = "Halo Polban"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+    val KONSTAN_DATA = "Data"
+
+//    dataInt = 6
+    dataIntVar = 8
+
+    println("Ini data dengan val $dataInt")
+    println("Ini data dengan val $dataIntVar")
+}
